69a2c34ad65470877a62849af25be8c86084af28
DevIhor <ihor.l@coaxsoft.com>
2022-08-23 13:02:02 +0300

    Remove caching image in QrCodeAPIMixin; Update QrCodeConfirmAPIMixin possible cases

--- qr/views.py
+++ qr/views.py
@@ -40,23 +40,14 @@ class QrCodeAPIMixin:
         except NoReverseMatch:
             return Response({'message': 'Redirect url is not found.'}, status=status.HTTP_404_NOT_FOUND)
 
-        user_id = request.user.id if hasattr(request.user, 'id') else -1  # Anonymous user will have id -1
-        if not redis_storage.hgetall(f"qr_{code_hash}"):
-            redis_storage.hmset(
-                f"qr_{code_hash}", QR_CODE_EXPIRATION_TIME, {'user_id': user_id,
-                                                             'redirect_url_name': self.REDIRECT_URL_NAME,
-                                                             'image': None})
-            img_data = None
-        else:
-            img_data = redis_storage.hgetall(f"qr_{code_hash}").get('image', None)
+        user_id = request.user.id if hasattr(request.user, 'id') else None  # Anonymous user will have id -1
+        redis_storage.hmset(f"qr_{code_hash}", QR_CODE_EXPIRATION_TIME, {'user_id': user_id,
+                                                                         'redirect_url_name': self.REDIRECT_URL_NAME})
 
-        if not img_data:
-            buffered = BytesIO()
-            img = make_qr_code(redirect_link)
-            img.save(buffered, format="PNG")
-            img_data = base64.b64encode(buffered.getvalue())
-            redis_storage.setex(f"qr_img_{code_hash}", QR_CODE_EXPIRATION_TIME, img_data)
-
+        buffered = BytesIO()
+        img = make_qr_code(redirect_link)
+        img.save(buffered, format="PNG")
+        img_data = base64.b64encode(buffered.getvalue())
         return Response({'qr': img_data}, status=status.HTTP_200_OK)
 
 
@@ -66,32 +57,53 @@ class QrCodeConfirmAPIMixin:
         redis_storage = kwargs['redis']
         code_hash = request.query_params.get('code_hash')
 
-        user_id = redis_storage.get(f"qr_{code_hash}")
+        qr_data = redis_storage.hgetall(f"qr_{code_hash}")
 
         # Process not existed QR
-        if not user_id:
+        if not qr_data:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-        # Process user QR with anonymous user
-        if user_id > 0 and not hasattr(request.user, 'id'):
-            return self.get_successful_response(request, *args, **kwargs)
+        qr_user_id = qr_data.get('user_id', None)
+        redirect_url_name = qr_data.get('redirect_url_name', None)
 
-        # Process user QR with the same authenticated user
-        if user_id > 0 and hasattr(request.user, 'id') and user_id == request.user.id:
-            return self.get_successful_response(request, *args, **kwargs)
+        # Process QR with wrong redirect url
+        if redirect_url_name != '':
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-        # Process user QR with another authenticated user
-        if user_id > 0 and hasattr(request.user, 'id') and user_id != request.user.id:
-            return Response(status=status.HTTP_403_FORBIDDEN)
+        # Process anonymous QR with anonymous user
+        if not qr_user_id and hasattr(request.user, 'id'):
+            return self.get_successful_response(request, qr_user_id, *args, is_login=False, **kwargs)
 
         # Process anonymous QR with authenticated user
-        if user_id == -1 and hasattr(request.user, 'id'):
-            return self.get_successful_response(request, *args, **kwargs)
+        if not qr_user_id and hasattr(request.user, 'id'):
+            return self.get_successful_response(request, qr_user_id, *args, is_login=True, **kwargs)
 
-    def get_successful_response(self, request, *args, **kwargs):
-        if self.confirm_qr_code(request, *args, **kwargs):
+        # Process user QR with anonymous user
+        if qr_user_id > 0 and not hasattr(request.user, 'id'):
+            return self.get_successful_response(request, qr_user_id, *args, is_login=True, **kwargs)
+
+        # Process user QR with the same authenticated user
+        if qr_user_id > 0 and hasattr(request.user, 'id') and qr_user_id == request.user.id:
+            return self.get_successful_response(request, qr_user_id, *args, is_login=False, **kwargs)
+
+        # Process user QR with another authenticated user
+        if qr_user_id > 0 and hasattr(request.user, 'id') and qr_user_id != request.user.id:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+    def get_successful_response(self, request, qr_user_id, *args, **kwargs):
+        # Process login
+        if kwargs.get('is_login', False):
+            response_data = self.confirm_qr_login(request, qr_user_id, *args, **kwargs)
+            if not response_data:
+                raise SyntaxError("You need to override `confirm_qr_login` method.")
+            return Response(status=status.HTTP_200_OK, data=response_data)
+        # Process other confirmation
+        if self.confirm_qr_code(request, qr_user_id, *args, **kwargs):
             return Response(status=status.HTTP_200_OK)
         raise SyntaxError("You need to override `confirm_qr_code` method.")
 
-    def confirm_qr_code(self, request, *args, **kwargs):
+    def confirm_qr_code(self, request, qr_user_id, *args, **kwargs):
+        return False
+
+    def confirm_qr_login(self, request, qr_user_id, *args, **kwargs):
         return False
